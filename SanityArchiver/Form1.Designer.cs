8e15aa66bd8f3f8a638c53b5b8565cb901732c0c
tarakarcsi <tarakarcsi94@gmail.com>
2019-08-21 12:11:44 +0200

    added listboy and file browser button

---
+++ SanityArchiver/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SanityArchiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.fileList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.AutoEllipsis = true;
+            this.BrowseButton.Location = new System.Drawing.Point(12, 12);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(110, 27);
+            this.BrowseButton.TabIndex = 0;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // fileList
+            // 
+            this.fileList.FormattingEnabled = true;
+            this.fileList.Location = new System.Drawing.Point(12, 60);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(364, 290);
+            this.fileList.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fileList);
+            this.Controls.Add(this.BrowseButton);
+            this.Name = "Form1";
+            this.Text = "Sanity Archiver";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.ListBox fileList;
+    }
+}
+
---
+++ SanityArchiver/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchiver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+
+            if(FBD.ShowDialog() == DialogResult.OK)
+            {
+                fileList.Items.Clear();
+                string[] files = Directory.GetFiles(FBD.SelectedPath);
+                string[] dirs = Directory.GetDirectories(FBD.SelectedPath);
+
+                foreach(string file in files)
+                {
+                    fileList.Items.Add(file);
+                }
+                foreach (string dir in dirs)
+                {
+                    fileList.Items.Add(dir);
+                }
+
+            }
+        }
+    }
+}
